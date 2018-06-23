e131fe070e40779a92542e0fac246db8ef1dfc2a
Marco <rossnmarco@gmail.com>
2018-06-23 15:32:28 -0400

    final clean up for the day

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ import csv
 # Marco Ross
 # Sheridan College - Bachelor of Applied Computer Science Undergraduate Thesis
 
-nltk.download('punkt')
+#nltk.download('punkt')
 
+# Import the text file we will work with
 with open('filteredtext.txt', 'r') as file:
     text = file.read().decode('utf-8')
 
-file = csv.writer(open('word_frequencies.csv', 'w'))
-file_bigrams = csv.writer(open('bigram_freq.csv', 'w'))
-
+# Tokenize the text
 tokens = word_tokenize(text)
 tokenized_text = nltk.Text(tokens)
 
@@ -28,6 +27,7 @@ stopwords = stopwords.words('english')
 # Define collection for set of words
 word_set = []
 
+# Remove stop words, remove punctuation, numbers and non words
 def normalize_text(tokenized_text):
     # Work through all the words in text and filter
     for word in tokenized_text:
@@ -38,7 +38,9 @@ def normalize_text(tokenized_text):
 
     return word_set
 
+# Get the word frequency (unigram) of each word
 def get_word_frequency():
+    file = csv.writer(open('word_frequencies.csv', 'w'))
 
     fd = FreqDist(word_set)
     #print fd.most_common(200)
@@ -49,8 +51,10 @@ def get_word_frequency():
     for key, count in fd.most_common(200):
         file.writerow([key.encode('utf-8'), count])
 
-
+# Get the bigrams of the words
 def get_bigrams():
+
+    file_bigrams = csv.writer(open('bigram_freq.csv', 'w'))
 
     # Define bigram and trigram measures
     #bigram_measures = nltk.collocations.BigramAssocMeasures()
@@ -70,6 +74,7 @@ def get_bigrams():
         #file_trigrams.writerow(type(bigram_tuple)(x.encode('utf-8') for x in bigram_tuple)) #just words without count
         file_bigrams.writerow([type(bigram_tuple)(x.encode('utf8') for x in bigram_tuple), count]) #formatted properly
 
+# Make function calls
 normalize_text(tokenized_text)
 get_word_frequency()
 get_bigrams()
